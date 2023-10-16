5761fa6cb35b40d467360022ecf47a728a14e295
dlytvynko1 <agrrbagrr1986@gmail.com>
2023-10-16 21:34:23 +0300

    transfer to PC

---
+++ main.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from collections import defaultdict 
+
+def get_birthdays_per_week(users):
+  birthdays_sorted = defaultdict(list)
+  for k, v in users:
+    birthdays_sorted[]
+  
+  current_date = datetime.today().date()
+  print(datetime(1955, 10, 28))
+
+get_birthdays_per_week([{"name": "Bill Gates", "birthday": datetime(1955, 10, 28)}, {"name": "Bill", "birthday": datetime(1925, 12, 22)}])
